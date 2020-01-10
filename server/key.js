991885a3a5ba7674f5e284eacd2ae149f59f7134
RylanFerrer <rylanferrer@gmail.com>
2020-01-10 15:33:10 -0800

    test push

--- server/key.js
+++ server/key.js
@@ -2,5 +2,5 @@ module.exports = {
     cloud_name: "Enter Cloudinary Cloud Name Here",
     api_key: "Enter Cloudinary API Key Here",
     api_secret: "Enter Cloudinary Secret API Key Here",
-    clarifai: "Enter Clarifai API Key Here"
+    clarifai: "Enter Clarifai API Key Here hello"
 }
