64a60d3697d398c0226683effd0aa6622cbf33a3
RylanFerrer <rylanferrer@gmail.com>
2020-01-09 17:41:53 -0800

    removed hardcoded good afternoon and time is now done dynamically

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,26 +1,33 @@
 import React, {useState} from 'react'
+import Header from './Header'
 import {Link} from 'react-router-dom'
 import axios from 'axios'
 export default function Search() {
     const [searchedUsers, setSearchedUsers] = useState(undefined)
+    
     const getResults = async(event) => {
-        if(event.target.value === '') {
+        if(event.target.value === '' || event.target.value === " ") {
             setSearchedUsers([])
         } else {
             const searchResults = await axios.get(`/feed/search/${event.target.value}`)
             setSearchedUsers(searchResults.data)
         }
-
     }
     return (
         <div>
-            <input onChange = {event => getResults(event)} ></input>
-            <div>
+            <div className = "nav__fit">
+                <Header  color = "white"/>
+            </div>
+            <div className = "search__input-container">
+                <input className = "search__input" onChange = {event => getResults(event)} ></input>
+            </div>
+   
+            <div className = "search__container">
                 {searchedUsers !== undefined ? searchedUsers.map((user,index) => {
                     return (
-                    <div key = {index}>
+                    <div className = "search__user" key = {index}>
                         <h4>{user.name}</h4>
-                        <Link to = {`/profile/${user._id}`}><img  alt = "profile" className = "feed__image" src = {user.profilePicture}/> </Link>
+                        <Link to = {`/profile/${user._id}`}><img  alt = "profile"   className = "search__image"  src = {user.profilePicture}/> </Link>
                     </div>
                     )
                 }): ""} 
