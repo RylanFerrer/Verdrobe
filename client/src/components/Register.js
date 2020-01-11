1f4a551cca7d9c8da0195a5965079a96c455161f
RylanFerrer <rylanferrer@gmail.com>
2020-01-10 17:50:48 -0800

    Added a function that validates uploaded files

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React,{useState} from 'react';
 import axios from 'axios'
 
 const Register = (props) => {
-
+    const [isUploaded, setIsUploaded] = useState(false)
     const handleSubmit = (event) => {
         event.preventDefault()
         const data = new FormData();
@@ -18,14 +18,29 @@ const Register = (props) => {
             }
         })
     }
+    const imageChecker = (event) => {
+        event.preventDefault()
+        const regex =  new RegExp("image");
+        //check if the file is an image
+        if(regex.test(event.target.files[0].type)) {
+            setIsUploaded(true)
+        } else {
+            event.target.value = null
+            alert('Please only upload images!')
+        }
+
+    }
+    const uploadState = isUploaded ? "Image is ready" : "Upload Profile Picture"
     return (
-        <div>
-            <form onSubmit = {(event) => handleSubmit(event)}>
-                <input name = "name" type = "name"  placeholder = "Name"required></input>
-                <input name = "email" type = "email"  placeholder = "E-mail"required></input>
-                <input name = "password" type = "password"  placeholder = "Password"required></input>
-                <input name = "file" type="file" id="file"/>
-                <label htmlFor="file" accept="image/*" className="btn-1" required>Upload Profile Picture</label>
+        <div className = "register__page">
+            <h1>Welcome To Verdrobe</h1>
+            <h4>Register Below</h4>
+            <form className = "register__form" onSubmit = {(event) => handleSubmit(event)}>
+                <input className = "register__form-input" name = "name" type = "name"  placeholder = "Name"required></input>
+                <input className = "register__form-input" name = "email" type = "email"  placeholder = "E-mail"required></input>
+                <input  className = "register__form-input" name = "password" type = "password"  placeholder = "Password"required></input>
+                <input onChange = {event => imageChecker(event)} name = "file"  accept="image/*" type="file" id="file"/>
+                <label  onChange = {event => console.log("change", event)} htmlFor="file" accept="image/*" className="btn-1" required>{uploadState}</label>
                 <button className="btn__upload" type="submit" name="button">Submit</button>
             </form>
         </div>
