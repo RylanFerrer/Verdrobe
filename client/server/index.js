e6b2bf9831a21a580936461ca381745766c53e7b
RylanFerrer <rylanferrer@gmail.com>
2020-01-20 23:02:30 -0800

    deployment build

--- client/server/index.js
+++ client/server/index.js
@@ -12,13 +12,13 @@ app.use(fileUpload({
 }));
 
 app.use(bodyParser.json());
-app.use(express.static(___dirname + './../client/build'));
+app.use(express.static(__dirname + './../build'));
 app.use(cookieParser());
 
 mongoose.connect("mongodb+srv://admin-rylan:Sonicrfx4+@cluster0-mw7yc.mongodb.net/verdrobeDB", {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
 
 app.get('*', (req, res) => {
-    res.sendFile('index.html',{root: ___dirname + './../client/build'});
+    res.sendFile('index.html',{root:  __dirname + './../build'});
 });
 app.use('/upload', require("./routes/api/upload"))
 app.use('/feed', require("./routes/api/feed"))
